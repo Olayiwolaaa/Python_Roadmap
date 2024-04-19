b0be63ccac66e75e15457b1e5707f11f69a1ab32
Olayiwolaaa <muizzkara91@gmail.com>
2024-04-19 23:16:32 +0100

    working with varibles

---
+++ variables.py
@@ -0,0 +1,14 @@
+# CREATING VARIBLES
+
+a = "Amhed"
+b = 30
+print(a)
+print(b)
+
+# CASTING
+
+# x = str(3) # using string casting
+# y = int(3) # using integer casting
+# z = float(3) # using floating casting
+
+# print(x,y,z) # printing variables x,y,z
