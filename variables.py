8f6dde550d21144ca2abf9fad6583d9ef2ef2db7
Olayiwolaaa <muizzkara91@gmail.com>
2024-04-19 23:21:19 +0100

    Successfully studie : Creating Varibles, Casting, Get Type

--- variables.py
+++ variables.py
@@ -1,14 +1,21 @@
-# CREATING VARIBLES
+# CREATING VARIBLES ✅
 
 a = "Amhed"
 b = 30
-print(a)
-print(b)
+# print(a)
+# print(b)
 
-# CASTING
+# CASTING ✅
 
-# x = str(3) # using string casting
-# y = int(3) # using integer casting
-# z = float(3) # using floating casting
+x = str(3) # using string casting
+y = int(3) # using integer casting
+z = float(3) # using floating casting
 
 # print(x,y,z) # printing variables x,y,z
+
+# TYPE ✅
+foo = True
+
+print(type(foo))
+
+# SINGLE or DOUBLE QUOTE ✅
